b4cc09e98811a5fe6a26a9ed442d7a3995913adb
Zenichi Amano <crow.misia@gmail.com>
2020-08-21 06:59:13 +0900

    use sndfile for wav file read/write. use soxr for sample rate convert.
    Signed-off-by: Zenichi Amano <crow.misia@gmail.com>

--- examples/rnnoise_demo.c
+++ examples/rnnoise_demo.c
@@ -26,35 +26,94 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <sndfile.h>
+#include <soxr.h>
+#include <sox.h>
 #include "rnnoise.h"
 
-#define FRAME_SIZE 480
+#define DENOISE_SAMPLE_RATE 48000
+#define DENOISE_FRAMES (DENOISE_SAMPLE_RATE / 1000 * 10)
+
+size_t resample(soxr_t soxr, const int16_t *in_buf, const int in_frames, int16_t *out_buf, const int out_frames) {
+    size_t odone;
+    const soxr_error_t error = soxr_process(soxr, in_buf, in_frames, NULL, out_buf, out_frames, &odone);
+    return error ? -1 : odone;
+}
+
+void denoise(soxr_t soxr_in, soxr_t soxr_out, int16_t *snd_buf, const int in_frames,
+             int16_t *resample_buf, float *denoise_buf, DenoiseState *st) {
+    resample(soxr_in, snd_buf, in_frames, resample_buf, DENOISE_FRAMES);
+
+    for (int i = 0; i < DENOISE_FRAMES; i++) {
+        denoise_buf[i] = (float) resample_buf[i];
+    }
+
+    rnnoise_process_frame(st, denoise_buf, denoise_buf);
+
+    for (int i = 0; i < DENOISE_FRAMES; i++) {
+        resample_buf[i] = denoise_buf[i];
+    }
+
+    resample(soxr_out, resample_buf, DENOISE_FRAMES, snd_buf, in_frames);
+}
 
 int main(int argc, char **argv) {
-  int i;
-  int first = 1;
-  float x[FRAME_SIZE];
-  FILE *f1, *fout;
-  DenoiseState *st;
-  st = rnnoise_create(NULL);
-  if (argc!=3) {
-    fprintf(stderr, "usage: %s <noisy speech> <output denoised>\n", argv[0]);
-    return 1;
-  }
-  f1 = fopen(argv[1], "rb");
-  fout = fopen(argv[2], "wb");
-  while (1) {
-    short tmp[FRAME_SIZE];
-    fread(tmp, sizeof(short), FRAME_SIZE, f1);
-    if (feof(f1)) break;
-    for (i=0;i<FRAME_SIZE;i++) x[i] = tmp[i];
-    rnnoise_process_frame(st, x, x);
-    for (i=0;i<FRAME_SIZE;i++) tmp[i] = x[i];
-    if (!first) fwrite(tmp, sizeof(short), FRAME_SIZE, fout);
-    first = 0;
-  }
-  rnnoise_destroy(st);
-  fclose(f1);
-  fclose(fout);
-  return 0;
+    DenoiseState *st;
+    SNDFILE *in_sf, *out_sf;
+    SF_INFO in_info = {0}, out_info = {0};
+    soxr_error_t error;
+
+    soxr_quality_spec_t const q_spec = soxr_quality_spec(SOXR_HQ, 0);
+    soxr_io_spec_t const io_spec = soxr_io_spec(SOXR_INT16_I, SOXR_INT16_I);
+    soxr_runtime_spec_t const runtime_spec = soxr_runtime_spec(sox_false);
+
+    st = rnnoise_create(NULL);
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <noisy speech> <output denoised>\n", argv[0]);
+        return 1;
+    }
+    in_sf = sf_open(argv[1], SFM_READ, &in_info);
+    out_info = (SF_INFO) {
+            .samplerate = in_info.samplerate,
+            .channels = 1,
+            .format = in_info.format
+    };
+    out_sf = sf_open(argv[2], SFM_WRITE, &out_info);
+
+    sf_command(in_sf, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+    sf_command(out_sf, SFC_SET_NORM_FLOAT, NULL, SF_FALSE);
+
+    const int in_frames = in_info.samplerate / 1000 * 10;
+    int16_t snd_buf[in_frames];
+    int16_t resample_buf[DENOISE_FRAMES];
+    float denoise_buf[DENOISE_FRAMES];
+
+    soxr_t soxr_in = soxr_create(in_info.samplerate, DENOISE_SAMPLE_RATE, 1, &error, &io_spec, &q_spec, &runtime_spec);
+    soxr_t soxr_out = soxr_create(DENOISE_SAMPLE_RATE, out_info.samplerate, 1, &error, &io_spec, &q_spec,
+                                  &runtime_spec);
+
+    const sf_count_t remain_frames = in_info.frames % in_frames;
+    while (1) {
+        const sf_count_t readSamples = sf_readf_short(in_sf, snd_buf, in_frames);
+        if (readSamples < in_frames) {
+            break;
+        }
+
+        denoise(soxr_in, soxr_out, snd_buf, in_frames, resample_buf, denoise_buf, st);
+        sf_writef_short(out_sf, snd_buf, in_frames);
+    }
+    if (remain_frames > 0) {
+        memset(snd_buf + (in_frames - remain_frames), 0, remain_frames);
+        denoise(soxr_in, soxr_out, snd_buf, in_frames, resample_buf, denoise_buf, st);
+        sf_writef_short(out_sf, snd_buf, remain_frames);
+    }
+
+    soxr_delete(soxr_in);
+    soxr_delete(soxr_out);
+    rnnoise_destroy(st);
+
+    sf_close(in_sf);
+    sf_close(out_sf);
+    return 0;
 }
